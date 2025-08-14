a640247ca0efdd3bc149c4af64000ed749413c8b
Keegan Erasmus <era.keegan@gmail.com>
2025-08-14 10:27:55 +0200

    Fixed Auth Styles to new format

--- apps/main-auth/src/app/layout.tsx
+++ apps/main-auth/src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import '@ui/styles/bootstrap.scss';
+import "@ui/styles/bootstrap/standard.scss";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
